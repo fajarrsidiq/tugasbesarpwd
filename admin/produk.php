d58101f83d208d56d3b51be774015052ed61e20a
fajarrsidiq <fsidiq596@gmail.com>
2025-06-18 08:20:18 +0700

    Mengedit file produk

--- admin/produk.php
+++ admin/produk.php
@@ -8,43 +8,71 @@ if(!isset($_SESSION['user_id']) || $_SESSION['level'] != 'admin') {
 }
 
 include '../../config/database.php';
+
+// Proses hapus produk
+if(isset($_GET['hapus'])) {
+    $id = $_GET['hapus'];
+    $query = "SELECT gambar FROM produk WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+    $product = mysqli_fetch_assoc($result);
+    
+    if($product) {
+        unlink("../../assets/images/".$product['gambar']);
+        $delete = "DELETE FROM produk WHERE id = $id";
+        mysqli_query($conn, $delete);
+        $_SESSION['success'] = "Produk berhasil dihapus";
+    }
+}
 ?>
 
 <section class="admin-products">
-    <h1>Kelola Produk</h1>
+    <div class="header">
+        <h1>Kelola Produk</h1>
+        <a href="tambah_produk.php" class="btn btn-tambah">
+            <i class="fas fa-plus"></i> Tambah Produk
+        </a>
+    </div>
     
-    <a href="tambah_produk.php" class="btn">Tambah Produk</a>
-    
-    <?php if(isset($_GET['success'])): ?>
-        <div class="alert success">Produk berhasil diupdate!</div>
+    <?php if(isset($_SESSION['success'])): ?>
+        <div class="alert success"><?php echo $_SESSION['success']; unset($_SESSION['success']); ?></div>
     <?php endif; ?>
     
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Gambar</th>
-            <th>Nama Produk</th>
-            <th>Harga</th>
-            <th>Aksi</th>
-        </tr>
-        <?php
-        $query = "SELECT * FROM produk";
-        $result = mysqli_query($conn, $query);
-        
-        while($row = mysqli_fetch_assoc($result)) {
-            echo '<tr>';
-            echo '<td>'.$row['id'].'</td>';
-            echo '<td><img src="../../assets/images/'.$row['gambar'].'" width="50"></td>';
-            echo '<td>'.$row['nama_produk'].'</td>';
-            echo '<td>Rp '.number_format($row['harga'], 0, ',', '.').'</td>';
-            echo '<td>';
-            echo '<a href="edit_produk.php?id='.$row['id'].'">Edit</a> | ';
-            echo '<a href="hapus_produk.php?id='.$row['id'].'" onclick="return confirm(\'Yakin ingin menghapus?\')">Hapus</a>';
-            echo '</td>';
-            echo '</tr>';
-        }
-        ?>
-    </table>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Gambar</th>
+                    <th>Nama Produk</th>
+                    <th>Harga</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $no = 1;
+                $query = "SELECT * FROM produk";
+                $result = mysqli_query($conn, $query);
+                
+                while($row = mysqli_fetch_assoc($result)): ?>
+                    <tr>
+                        <td><?= $no++; ?></td>
+                        <td><img src="../../assets/images/<?= $row['gambar']; ?>" width="50"></td>
+                        <td><?= $row['nama_produk']; ?></td>
+                        <td>Rp <?= number_format($row['harga'], 0, ',', '.'); ?></td>
+                        <td class="actions">
+                            <a href="edit_produk.php?id=<?= $row['id']; ?>" class="btn btn-edit">
+                                <i class="fas fa-edit"></i> Edit
+                            </a>
+                            <a href="produk.php?hapus=<?= $row['id']; ?>" class="btn btn-hapus" onclick="return confirm('Yakin ingin menghapus?')">
+                                <i class="fas fa-trash"></i> Hapus
+                            </a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    </div>
 </section>
 
 <?php include '../../includes/footer.php'; ?>
